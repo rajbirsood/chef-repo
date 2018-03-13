a0dd1c256a878308f9b8873c2d59556511fcb827
root <root@test01.edureka.com>
2018-03-13 18:31:25 +0530

    initial commit

---
+++ .chef/local-mode-cache/cache/cookbooks/myapache/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook:: myapache
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package "apache2" do
+	action :install
+end
+
+service "apache2" do
+	action [:enable, :start]
+end
